7b95689c4a3fff532793cf733f0f4cabb7ccbd1c
alamin-7 <alamin188914@gmail.com>
2026-01-19 19:03:20 +0600

    init

---
+++ src/main/java/com/example/groundedai/GroundedaiApplication.java
@@ -0,0 +1,13 @@
+package com.example.groundedai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroundedaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroundedaiApplication.class, args);
+	}
+
+}
